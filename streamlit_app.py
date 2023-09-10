579aa7721476a97bfc6dc1081cf619f741db1b0a
Hideki Ono <unikarei@gmail.com>
2023-09-10 13:34:23 +0900

    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ import threading
 
 time_limit = 8  # application time limit.
 
-def shutdown():
-    os._exit(0)
+#def shutdown():
+#    os._exit(0)
 
-def close_streamlit_after_delay():
-    threading.Timer(time_limit, shutdown).start()
+#def close_streamlit_after_delay():
+#    threading.Timer(time_limit, shutdown).start()
 
 def main():
     st.title("TEST")
